c212c7ea40acc2f28d2e4e788d7e63ff42933e89
Juan Carlos Obando Mora <jcobandom1@gmail.com>
2021-12-20 17:23:50 +0000

    Respaldo

--- sdk.py
+++ sdk.py
@@ -4,3 +4,5 @@ api = WebexTeamsAPI(access_token="NDQzYjFmNDItNTRhYy00MjliLTkzMjctM2M3NDE1ODFkNz
 room_id = "Y2lzY29zcGFyazovL3VzL1JPT00vYzg2ZjljMDAtNTY5Yi0xMWVjLThjNmUtYjE2MmM5MjUxYmVl"
 
 api.messages.create(roomId = room_id, text = "Enviado desde el SDK de Python")
+
+
